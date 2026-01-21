bb3822c1333df47a0d008f4ba8399ce03f48e586
kahweng <ngkahweng2003@gmail.com>
2026-01-21 16:51:30 +0800

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nkahweng.github.io/",
+  base: "/sunshine-enrichment-centre/",
 });
